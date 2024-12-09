51a38d15756fed10612fdbfcf50aceafdf35e658
Nishatul Majid <pieces9909@gmail.ocm>
2024-12-08 22:08:43 -0500

    Starting the flask project

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 print('hello')
 print("many random things2")
-# from flask import Flask
+from flask import Flask
 
-# app = Flask(__name__)
+app = Flask(__name__)
 
-# @app.route('/')
-# def home():
-#     return "Welcome to your Flask backend!"
+@app.route('/')
+def home():
+    return "Welcome to your Flask backend!"
 
-# if __name__ == "__main__":
-#     app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
 
