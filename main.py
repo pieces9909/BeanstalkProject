11b7fb7cbd218b634d8ada1503611c4c54d271f7
Nishatul Majid <pieces9909@gmail.ocm>
2024-12-08 21:03:04 -0500

    Added .gitignore file

---
+++ main.py
@@ -0,0 +1,13 @@
+print('hello')
+
+# from flask import Flask
+
+# app = Flask(__name__)
+
+# @app.route('/')
+# def home():
+#     return "Welcome to your Flask backend!"
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
+
